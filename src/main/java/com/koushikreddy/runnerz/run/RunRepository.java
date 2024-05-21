cdbe0dcdcb845e3e18b271bd6053bd6294b9757c
Koushik Reddy Gajjala <kgajjala@csuchico.edu>
2024-05-21 12:08:00 -0700

    added CRUD operations to in-memory data

--- src/main/java/com/koushikreddy/runnerz/run/RunRepository.java
+++ src/main/java/com/koushikreddy/runnerz/run/RunRepository.java
@@ -2,16 +2,43 @@ package com.koushikreddy.runnerz.run;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
 import java.time.LocalDateTime;
 
 import jakarta.annotation.PostConstruct;
 
+@Repository
 public class RunRepository {
 
     private List<Run> runs = new ArrayList<>();
 
     List<Run> findAll() {
         return runs;
+    }
+
+    public Optional<Run> findById(int id) {
+        return runs.stream().filter(run -> run.id() == id).findFirst();
+    }
+
+    void create(Run run) {
+        runs.add(run);
+    }
+
+    void update(Run run, Integer id) {
+        Optional<Run> existingRun = findById(id);
+        if (existingRun.isPresent()) {
+            runs.set(runs.indexOf(existingRun.get()), run);
+        }
+    }
+
+    void delete(Integer id) {
+        Optional<Run> existingRun = findById(id);
+        if (existingRun.isPresent()) {
+            runs.remove(existingRun.get());
+        }
     }
 
     @PostConstruct
